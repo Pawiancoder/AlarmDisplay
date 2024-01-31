9745a37b88ff1916bd07ae6e6b91fa1a9e14e8e8
Pawiancoder <maksiuoechsler@outlook.com>
2024-01-31 16:35:48 +0100

    Bugfix start
    Error but alarm

--- UserIputs/user.js
+++ UserIputs/user.js
@@ -33,94 +33,44 @@ alarmBtn.addEventListener("click", () => {
 })
 
 function getJsonData() {
-    let brandalarm = false;
-    let thl = false;
-    let vehicles = [];
+    let brandalarmState = document.getElementById("fire").checked;
+    let thlState = document.getElementById("thl").checked;
     let jsonData = [];
-    let checkedAlarmBoxes = getCheckedBoxes("alarmTypes");
-    let checkedVehicles = getCheckedBoxes("vehicles");
-    let gongPlaceHoler = 0;
-    //Code
-    if (checkedAlarmBoxes.length >= 2 || checkedAlarmBoxes.length == 0 && !resetCheckbox.checked) {
-        alert("Bitte maximal eine Alarmart auswählen!");
-        throw new Error("Bitte nur eine Alarmart auswählen!");
-    } else if (resetCheckbox.checked) {
-        //!reset
-        jsonData =
-        {
-            title: "RESET",
-            vehicles: ["REI44"],
-            category: "THL",
-            sound: false,
-            type: 1,
-            status: true
-        }
-    } else {
-        if (checkedVehicles.length == 0) {
-            alert("Es wurden keine Fahrzeuge ausgewählt!");
-        } else {
-            if (keyWordField.value == "") {
-                alert("Es wurde kein Stichwort eingegeben!");
-            } else {
-                if (gongPlaceHoler > 1) {
-                    alert("unerwarteter Fehler! Bitte den Entwickler informieren (Fehlercode: Maschin' kaputt!");
-                    throw new Error("user.js (UserScript) 0 > 1 is true geworden!");
-                } else {
-                    let kategorie = checkedAlarmBoxes[0].toString();
-                    if (soundRequired.checked) {
-                        //gong an
-                        console.log("K: ", kategorie);
-                        if (gongtype_fire.checked) {
-                            jsonData = {
-                                title: keyWordField.value,
-                                vehicles: checkedVehicles,
-                                category: kategorie,
-                                sound: true,
-                                type: 2,
-                                status: false
-                            }
-                        } else {
-                            jsonData = {
-                                title: keyWordField.value,
-                                vehicles: checkedVehicles,
-                                category: kategorie,
-                                sound: true,
-                                type: 1,
-                                status: false
-                            }
-                        }
-                    } else {
-                        //gong aus
-                        if (gongtype_fire.checked) {
-                            jsonData = {
-                                title: keyWordField.value,
-                                vehicles: checkedVehicles,
-                                category: kategorie,
-                                sound: false,
-                                type: 2,
-                                status: false
-                            }
+    let DispatchedVehicles = [];
+    let vehicleChecks = 0;
 
-                        } else {
-                            jsonData =
-                            {
-                                title: keyWordField.value,
-                                vehicles: checkedVehicles,
-                                category: kategorie,
-                                sound: false,
-                                type: 1,
-                                status: false
-                            }
-                        }
-                    }
-                }
-            }
+    let vehicles = [];
+    let checkedVehicles = document.querySelectorAll('.vehicles input[type="checkbox"]');
+
+    checkedVehicles.forEach(vehicle => { //Schaut, welche Fahrzeuge ausgewählt wurden
+        if (vehicle.checked) {
+            vehicleChecks++;
+            DispatchedVehicles.push(vehicle.name);
         }
+    });
+
+    if (vehicleChecks == 0) {
+        alert("Fahrzeuge müssen ausgewählt werden!");
     }
-    alert("Alarmiert! Bitte am Monitor nach der Alarmierung zurücksetzen und dann bei Bedarf erst erneut alarmieren!");
+
+    if (brandalarmState || thlState) { //Wenn nur eine Alarmart ausgewählt wurde geht es weiter
+        if (brandalarmState && thlState) {
+            alert("Es darf nur eine Alarmart ausgewählt werden!");
+        }
+    } else if (!brandalarmState && !thlState) {
+        alert("Es wurde keine Alarmart ausgewählt!");
+    }
+
+
+
+    /**
+     * alert("Alarmiert! Bitte am Monitor nach der Alarmierung zurücksetzen und dann bei Bedarf erst erneut alarmieren!");
     sendPostRequest(jsonData);
     resetFields();
+     */
 }
+
+
 
 function getCheckedBoxes(type) { //types: vehicles; alarmTypes; gong
     let returnArr = [];
