2160620f7f41d676d0b6e57960cf7774bf527bcd
Pawiancoder <maksiuoechsler@outlook.com>
2024-02-02 17:01:52 +0100

    CSS Updated
    Distance VehicleList and AlarmTitleField

--- apiCalls.js
+++ apiCalls.js
@@ -3,7 +3,6 @@ const ip = "raspberrypi"; //Hostname
 const apiUrl = `http://${ip}:5000/api/alarm-api`;
 
 function sendGETrequest(reset) {
-    debugger;
     console.log("CALLED!");
     if (!activeAlert) {
         fetch(apiUrl)
